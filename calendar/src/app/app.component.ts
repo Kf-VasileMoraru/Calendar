7cbea45ed38f9ff1d4292b7d989f0c30b579afdf
Vasile Moraru <chisinau2000@gmail.com>
2025-03-12 18:21:32 +0200

    + fix ...

--- calendar/src/app/app.component.ts
+++ calendar/src/app/app.component.ts
@@ -6,22 +6,8 @@ import { MatSlideToggle } from '@angular/material/slide-toggle';
   selector: 'app-root',
   imports: [RouterOutlet, MatSlideToggle],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'calendar ';
-}
-
-function calculateTotal(items: number[], discount: number) {
-  let total = 0;
-  const taxRate = 0.1;   // Unused variable
-  for (let i = 0; i < items.length; i++) {  // Inconsistent spacing, missing semicolon
-    total += items[i];
-  }
-
-  if (discount > 0)  // No block statement, could lead to errors
-    console.log('Discount applied');
-  total -= discount;
-
-  return total;
 }
