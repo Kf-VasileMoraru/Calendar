c09ab12649b3818d450f634ae529218f2acc8708
Vasile Moraru <chisinau2000@gmail.com>
2025-04-19 19:07:34 +0300

    add theme

--- calendar/src/app/app.routes.ts
+++ calendar/src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   loadComponent: () => import('./features/home/home.component').then((mod) => mod.HomeComponent),
+  // },
+  // {
+  //   path: 'profile',
+  //   loadComponent: () =>
+  //     import('./features/profile/profile.component').then((mod) => mod.ProfileComponent),
+  //   canActivate: [AuthGuard],
+  // },
+  // {
+  //   path: 'public',
+  //   loadComponent: () =>
+  //     import('./features/public/public.component').then((mod) => mod.PublicComponent),
+  // },
+  // {
+  //   path: 'protected',
+  //   loadComponent: () =>
+  //     import('./features/protected/protected.component').then((mod) => mod.ProtectedComponent),
+  //   canActivate: [AuthGuard],
+  // },
+  // {
+  //   path: 'admin',
+  //   loadComponent: () =>
+  //     import('./features/admin/admin.component').then((mod) => mod.AdminComponent),
+  //   canActivate: [AuthGuard],
+  // },
+  // {
+  //   path: 'callback',
+  //   loadComponent: () =>
+  //     import('./features/callback/callback.component').then((mod) => mod.CallbackComponent),
+  // },
+  // {
+  //   path: '**',
+  //   loadComponent: () =>
+  //     import('./features/not-found/not-found.component').then((mod) => mod.NotFoundComponent),
+  // },
+];
